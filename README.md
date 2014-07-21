3b3868283cecc187c2dc1941c016084d3c91e482
Cyberrebell <chainsaw75@web.de>
2014-07-21 23:38:48 +0200

    basics

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ ZF2ValidatorBuilder
 ===================
 
 Provides an easy to use Form-Validation-Toolset
+
+
+coming soon:
+
+-more validators
+
+-equivalient frontend validation
+
+-unit-tests
+
+-documentation
---
+++ library/ZF2ValidatorBuilder/src/ZF2ValidatorBuilder/ValidatorBuilder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ZF2ValidatorBuilder;
+
+class ValidatorBuilder
+{
+    /**
+     * Set element required: frontend & backend validation
+     * @param \Zend\Form\Element $element
+     */
+    public function setElementRequired(\Zend\Form\Element $element) {
+        $element->setAttribute('required', 'true'); //set browser validation
+        $this->getInputFilter()->get($element->getAttribute('name'))->setAllowEmpty(false); //set backend requirement
+    }
+    
+    public function attachEmailAddressValidator(\Zend\Form\Element $element) {
+        $elementValidatorChain = $this->getElementValidatorChain($element);
+        $elementValidatorChain->attachByName('EmailAddress');
+    }
+    
+    public function attachEqualsValidator(\Zend\Form\Element $element, $value) {
+        $elementValidatorChain = $this->getElementValidatorChain($element);
+        $elementValidatorChain->attachByName('Regex', ['pattern' => '/' . $value . '/']);
+    }
+    
+    public function attachIdenticalValidator(\Zend\Form\Element $elementOne, \Zend\Form\Element $elementTwo) {
+        $elementTwoValidatorChain = $this->getElementValidatorChain($elementTwo);
+        $elementTwoValidatorChain->attachByName('Identical', ['token' => $elementOne->getAttribute('name'), 'strict' => true]);
+    }
+    
+    public function attachStringLengthValidator(\Zend\Form\Element $element, $min = NULL, $max = NULL) {
+        $elementValidatorChain = $this->getElementValidatorChain($element);
+        $options = [];
+        if ($min !== NULL) {
+            $options['min'] = $min;
+        }
+        if ($max !== NULL) {
+            $options['max'] = $max;
+        }
+        $elementValidatorChain->attachByName('StringLength', $options);
+    }
+    
+    /**
+     * Get elements validator chain
+     * @param \Zend\Form\Element $element
+     * @return \Zend\Validator\ValidatorChain
+     */
+    protected function getElementValidatorChain(\Zend\Form\Element $element) {
+        $elementName = $element->getAttribute('name');
+        return $this->getInputFilter()->get($elementName)->getValidatorChain();
+    }
+}
